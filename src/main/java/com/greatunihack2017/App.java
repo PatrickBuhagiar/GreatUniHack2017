ea3b5f8c3ee2d8623f5e6c66566a9374fc05b530
sofaw <sew56@cam.ac.uk>
2017-11-10 17:14:41 +0000

    Add maven project.

---
+++ src/main/java/com/greatunihack2017/App.java
@@ -0,0 +1,13 @@
+package com.greatunihack2017;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
