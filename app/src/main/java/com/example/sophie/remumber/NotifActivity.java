a56ac5e8a959420ad0bf1792f199296e61444964
sofaw <sew56@cam.ac.uk>
2017-11-12 11:01:30 +0000

    Add weather things.

--- app/src/main/java/com/example/sophie/remumber/NotifActivity.java
+++ app/src/main/java/com/example/sophie/remumber/NotifActivity.java
@@ -8,6 +8,7 @@ import android.telephony.SmsManager;
 
 public class NotifActivity extends Activity {
     public static final String SMS_INTENT_ID_KEY = "com.example.remumber.KEY_SMS";
+    public static String NOTIF_TYPE = "com.example.remumber.NOTIF_ID";
     public static String NAME_KEY =  "com.example.remumber.NAME_SMS";
     public static String LOCATION_KEY = "com.example.remumber.LOCATION_SMS";
     public static String PHONE_KEY = "com.example.remumber.NUMBER_SMS";
@@ -18,10 +19,15 @@ public class NotifActivity extends Activity {
         // Send sms
         Intent launchIntent = getIntent();
         SmsManager sms = SmsManager.getDefault();
-        String message = "Hi " + launchIntent.getStringExtra(NAME_KEY) +
-                ", I've just arrived in " + launchIntent.getStringExtra(LOCATION_KEY) + "!";
-        sms.sendTextMessage(launchIntent.getStringExtra(PHONE_KEY), null,
-                message, null, null);
+        int notifType = launchIntent.getIntExtra(NOTIF_TYPE, 1);
+        if(notifType == HomeActivity.locationNotifId) {
+            String message = "Hi " + launchIntent.getStringExtra(NAME_KEY) +
+                    ", I've just arrived in " + launchIntent.getStringExtra(LOCATION_KEY) + "!";
+            sms.sendTextMessage(launchIntent.getStringExtra(PHONE_KEY), null,
+                    message, null, null);
+        } else if(notifType == HomeActivity.weatherNotifId) {
+            System.out.println("TODO: send weather message");
+        }
         Intent intent = new Intent(this.getApplicationContext(), HomeActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/sophie/remumber/Weather.java
+++ app/src/main/java/com/example/sophie/remumber/Weather.java
@@ -1,8 +1,85 @@
 package com.example.sophie.remumber;
 
+import android.app.Activity;
+import android.location.Location;
+import android.os.AsyncTask;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by sophie on 12/11/2017.
  */
 
 public class Weather {
+
+    public static void getWeather(Location location) {
+        //String locString = HomeActivity.mLocationField.getText().toString();
+
+        String locString = "York";
+
+
+        // HTTP request to get weather from latitude and longitude
+        HttpURLConnection connection = null;
+        //String urlParameters = locString;
+        String targetURL = "http://api.openweathermap.org/data/2.5/weather?q=" + locString;
+
+        try {
+            //Create connection
+            URL url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            /*connection.setRequestProperty("Content-Type",
+                    "application/x-www-form-urlencoded");*/
+
+            /*connection.setRequestProperty("Content-Length",
+                    Integer.toString(urlParameters.getBytes().length));*/
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoOutput(true);
+
+            //Send request
+            /*DataOutputStream wr = new DataOutputStream (
+                    connection.getOutputStream());
+            wr.writeBytes(urlParameters);
+            wr.close();*/
+
+            //Get Response
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+            String line;
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            String responseString = response.toString();
+            WeatherResponse weatherInfo = new ObjectMapper().readValue(responseString, WeatherResponse.class);
+            HomeActivity.weatherResponse =  weatherInfo;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+
+    public static class WeatherResponse {
+        public String weather;
+        public String main;
+        public String description;
+        public String temp;
+        public Object coord;
+    }
 }
